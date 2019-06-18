f326aea769ff992a5a5ad85cddd6edcc7dd26c21
Blake Stoner <stonerblake@gmail.com>
2019-06-18 13:33:24 -0400

    seeds

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+require_relative "..models/student.rb"
+
+Student.create(name: "Blake Stoner")
+Student.create(name: "Alex Siler")
+Student.create(name: "Christopher Swofford")
+Student.create(name: "Christopher Kalfas")
+Student.create(name: "Maryam Joseph")
+
+Module.create(name: "Module 1")
+Module.create(name: "Module 2")
+Module.create(name: "Module 3")
+Module.create(name: "Module 4")
+Module.create(name: "Module 5")
+
+StudyBuddy.create(name: "Jarvis")
+StudyBuddy.create(name: "Friday")
+StudyBuddy.create(name: "Cortona")
+StudyBuddy.create(name: "Jarvis")
+StudyBuddy.create(name: "Hal")
