57f762a209d0c3a77109bf0ade6ce07e8dab5370
Blake Stoner <stonerblake@gmail.com>
2019-06-19 14:25:18 -0400

    update -working on delete

--- app/models/studybuddy.rb
+++ app/models/studybuddy.rb
@@ -4,13 +4,28 @@ class StudyBuddy < ActiveRecord::Base
     has_many :notes
 
 def find_by_subject(subject)
-the_note = Note.all.select do |n|
-  n.subject == subject
-    end
-    the_note.map do |m|
-      m.title
-    end
+  the_note = Note.all.select do |n|
+    n.subject == subject
   end
+  the_note.map do |m|
+    m.title
+  end
+end
+
+def find_by_title(title)
+the_notes = Note.all.select do |n|
+  n.title == title
+  end
+  the_notes
+end
+
+def destroy_by_title(title)
+# the_notes = Note.all.select do |n|
+#   n.title == title
+#   end
+#   the_notes.delete_all
+Note.where(study_buddy_id: self.id).where(title: title).delete_all
+end
 
   def find_by_date(date)
     the_date_note = Note.all.select do |d|
