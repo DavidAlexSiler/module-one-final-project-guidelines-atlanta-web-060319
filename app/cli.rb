0db1e17b33506956c4b3e16e827c5ccbd84ded01
github username <github email address>
2019-06-20 23:36:11 -0400

    added read and write note

--- app/cli.rb
+++ app/cli.rb
@@ -7,8 +7,7 @@ class CLI
         
         @prompt = TTY::Prompt.new
         @pastel = Pastel.new
-        @student = nil
-        # @mod = Mod.create(name: "Mod 1", question: "what is my name")
+        # @student = nil
         
         
         login
@@ -24,6 +23,7 @@ class CLI
         if choice == choices[0]
             puts "Please enter your username:"
             return_user = gets.chomp
+            @student = Student.all.find {|a| a.name == return_user}
         elsif choice == choices[1]
             new_user_setup
         end
@@ -39,7 +39,7 @@ class CLI
             mod_choice = @prompt.enum_select("Select one:", mod_choices)
             if mod_choice == mod_choices[0]
                 puts "Welcome to Mod 1, you are gonna be great!"
-                @mod = Mod.create(name: "Mod 1", question: "What is your name?")
+                @mod = Mod.create(name: "Mod 1", question: "#{plucked_q}")
             else 
                 puts "ACCESS DENIED\nYou currently only have access to Mod 1"
                 mod_choice = @prompt.enum_select("Select one:", mod_choices)
@@ -48,96 +48,15 @@ class CLI
             make_buddy
         end
 
-        # def this_user_mod
-        #     make_a_mod = Mod.create(name: "Mod 1", question: "What is your name?")
-        # end
+        def plucked_q
+            Mod.pluck(:question).limit(1)
+        end
 
         def make_buddy
         puts "Now give your StudyBuddy a name:"
         this_sb_name = gets.chomp
         @studybuddy = StudyBuddy.create(name: "#{this_sb_name}", mod_id: @mod.id, student_id: @student.id, quiz_score: nil)
         end
-        # def get_user_id
-        #     this_user_id = this_user.map do |u|
-        #         u.id
-        #     end
-        #     this_user_id
-        # end
-
-        # def get_mod_id
-        #     this_mod_id = mod_choice.map do |e|
-        #         e.id
-        #     end
-        #     this_mod_id
-        # end
-
-        def buddy_setup
-            puts "Now give your StudyBuddy a name:"
-            this_sb_name = gets.chomp
-            binding.pry
-            this_user_study_buddy = StudyBuddy.create(name: "#{this_sb_name}", mod_id: this_user_mod.id, student_id: username.id, quiz_score: nil)
-        end
-
-            # while mod_choice == gets.chomp
-            #     case mod_choice 
-            #     when mod_choice == mod_choices[0]
-            #         #APPLAUSE
-            #         puts "Welcome to Mod 1, you are gonna be great!"
-            #         break
-            #     else puts "ACCESS DENIED\nYou currently only have access to Mod 1"
-            #         mod_choice
-            #     end
-            # end
-            
-        
-        
-        # if Student.all.includes("this_user.name")
-            # puts "Welcome back #{@student.name}! \n\n Loading up your study buddy now."
-        # else
-        #     puts "Thank you for choosing StuddyBuddy! \n We will now have you set up your new Buddy."
-        #     @student = Student.create(name: "#{}")
-        #     sleep 1
-        #     `reset`
-        #     new_bud
-        #     def new_bud
-        #     end
-        # end
-    
-        
-    #     puts "Hello #{@student.name}! I am your new StuddyBuddy. \n\nWould you give me a name?"
-    #     buddy_name = gets.chomp
-    #     StudyBuddy.create(name: "#{buddy_name}", mod_id: @mod.id, student_id: @student.id, quiz_score: nil)
-    #     #loading animation
-    #     sleep 1
-    #     `reset`
-    #     puts "\nWARNING WARNING DOES NOT COMPUTE\nDOOMSDAY KEYWORD ENTERED\nSELF DISTRUCT ACTIVATED IN"
-    #     puts "29"
-    #     sleep 2
-    #     puts "28"
-    #     sleep 2
-    #     puts "27"
-    #     sleep 2
-    #     puts "26"
-    #     sleep 2
-        
-    #     puts "I'm just kidding! #{buddy_name} isn't my Doomsday Keyword, it's 'brit'! \n\n
-    #     Just DO NOT under ANY circumstances enter the keyword 'brit'. REALLY!"
-    #     sleep 5
-    #     `reset`
-    # end
-
-            # def introduction
-            #     puts "Thank you #{@student.name}! \n\nI like my new name, I always thought I looked like a #{@studybuddy.name}."
-            # end
-
-            # introduction
-            
-        
-        
-    
-
-
-    
 
     def command
         sleep 1
@@ -158,7 +77,9 @@ class CLI
         elsif choice == choices[2]
             delete_note
         elsif choice == choices[4]
+            `reset`
             puts "Thanks for using your StudyBuddy!.\n\n"
+            #animation
             sleep 3
             exit
             end
@@ -176,9 +97,40 @@ class CLI
         puts "subject: #{subject}\ntitle: #{title}\ndate: #{date}"
         puts "\n\nSay whatevers on your mind!"
         content = gets.chomp
-        Note.create(subject: "#{subject}", title: "#{title}", date: "#{date}", content: "#{content}", study_buddy_id: @studybuddy.id)
+        @studybuddy.create_note(subject: "#{subject}", title: "#{title}", date: "#{date}", content: "#{content}")
+        sleep 1
+        `reset`
+        main_menu
     end
 
+    def read_note
+        puts "You can search for a note by its subject, title, or by date.\n\nWhat are you looking for?"
+        choices = ['Subject', 'Title', 'Date', 'Exit']
+        choice = @prompt.enum_select("Select one.", choices)
+        if choice == choices[0]
+            puts "Alright #{@student.name}, what subject?"
+            sub_search = gets.chomp
+            found = @studybuddy.notes.find_by_subject(sub_search)
+            puts "Here are the contents of the note:\n\n Subject: #{sub_search}\n\n\n#{found.content}"
+        elsif choice == choices[1]
+            puts "Okay #{@student.name}, what title?"
+            tit_search = gets.chomp
+            found = @studybuddy.notes.find_by_title(tit_search)
+            found.content
+            puts "Here are the contents of the note:\n\n Subject: #{sub_search}\n\n\n#{found.content}"
+        elsif choice == choices[2]
+            puts "You got it #{@student.name}, what was the date? \n dont forget to/fo/rmat!"
+            dat_search = gets.chomp
+            found = @studybuddy.notes.find_by_date(dat_search)
+            found.content
+            puts "Here are the contents of the note:\n\n Subject: #{sub_search}\n\n\n#{found.content}"
+        else
+            main_menu
+        end
+    
+    
+
+    end
         
 
 end
